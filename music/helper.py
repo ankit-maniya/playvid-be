20fc189c35c815001300c42a96f52cdd705419fa
Ankit Maniya <ankit.maniya6588@gmail.com>
2022-10-31 16:37:31 +0530

    Initial commit

---
+++ music/helper.py
@@ -0,0 +1,111 @@
+import os
+import json
+
+
+def extrect_formate_data(formate_data):
+    url = formate_data["url"]
+    format = formate_data["format"]
+    ext = formate_data["ext"]
+
+    orignal_formate = ["video"]
+    if (formate_data.get("format_note")):
+        orignal_formate = formate_data["format_note"]
+
+    filesize = 0
+    if (formate_data.get("filesize")):
+        filesize = formate_data["filesize"] / 1000000
+
+    return {
+        "url": url,
+        "ext": ext,
+        "orignal_formate": orignal_formate,
+        "filesize": filesize,
+        "format": format
+    }
+
+
+def extract_video_data_from_url(video_data):
+    title = video_data["title"]
+    formats = video_data["formats"]
+    thumbnails = video_data["thumbnails"][-1]
+    all_formats = [extrect_formate_data(
+        formate_data) for formate_data in formats]
+
+    return {
+        "formats": all_formats,
+        "thumbnails": thumbnails,
+        "title": title
+    }
+
+
+def formate_ext(mimetype):
+    ext = mimetype.split(";", 1)[0]
+    if (ext):
+        ext = ext.split("/")
+
+    return ext
+
+
+def add_ext(data):
+    newFormats = []
+    newAdaptiveFormats = []
+    formats = data["formats"]
+    adaptiveFormats = data["adaptiveFormats"]
+
+    for y in formats:
+        ext1 = formate_ext(y["mimeType"])
+        # size = int(y["contentLength"]) / 1000000
+        newFormats.append({**y,  "ext": ext1})
+
+    for x in adaptiveFormats:
+        ext = formate_ext(x["mimeType"])
+        # size = int(x["contentLength"]) / 1000000
+        newAdaptiveFormats.append({**x,  "ext": ext})
+
+    return {
+        "formats": newFormats,
+        "adaptiveFormats": newAdaptiveFormats
+    }
+
+
+def extract_facebook_video_data_from_url(video_data):
+    formats = video_data["formats"]
+    all_formats = [
+        formate_data for formate_data in formats if formate_data.get("format_id") in ["sd", "hd"]]
+    # result = [number for number in numbers if number > 5]
+    # [function(number) for number in numbers if condition(number)]
+
+    return {
+        "formats": all_formats,
+    }
+
+
+def extract_bandcamp_video_data_from_url(video_data):
+    return {
+        "id": video_data["id"],
+        "uploader": video_data["uploader"],
+        "artist": video_data["artist"],
+        "url": video_data["url"],
+        "album": video_data["album"],
+        "fulltitle": video_data["fulltitle"],
+        "title": video_data["title"],
+        "format": video_data["format"],
+        "thumbnail": video_data["thumbnail"],
+        "track": video_data["track"],
+        "ext": video_data["ext"],
+        "timestamp": video_data["timestamp"],
+        "duration": video_data["duration"],
+    }
+
+
+def get_arr_of_obj(array):
+    iResponse = []
+
+    for data in array:
+        if data:
+            updatedStr = data+"}"
+            output = json.loads(updatedStr)
+            # formateObj = extract_bandcamp_video_data_from_url(output) #It is taking too much time that's why I ignore filtering data
+            iResponse.append(output)
+
+    return iResponse
---
+++ music/views.py
@@ -0,0 +1,111 @@
+import json
+import os
+import metadata_parser
+from pytube import YouTube
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from .helper import add_ext, extract_bandcamp_video_data_from_url, extract_facebook_video_data_from_url, extract_video_data_from_url, get_arr_of_obj
+
+
+class MusicApiView(APIView):
+    """
+    View to list all youtube in the system.
+
+    * Requires token authentication.
+    * Only admin youtube are able to access this view.
+    """
+
+    def post(self, request, format=None):
+        """
+        Return a list of all youtube videos/audio.
+        """
+        url = request.data.get("url")
+        if url:
+            yt = YouTube(url)
+            iRes = yt.streaming_data
+            thumbnail = yt.thumbnail_url
+            title = yt.title
+            iRes = add_ext(iRes)
+
+            # Doownload Video From Youtube-DL
+            # command = f'youtube-dl "{url}" -j'
+            # output = os.popen(command).read()
+            # video_data = json.loads(output)
+            # iRes = extract_video_data_from_url(video_data)
+
+            return Response({"thumbnail": thumbnail, "title": title, **iRes})
+        return Response({"msg": "Please Provide Link"})
+
+    @api_view(['post'])
+    def get_facebook_videos(request, format=None):
+        """
+            Return a list of all users.
+            Doownload Video From Youtube-DL
+        """
+        url = request.data.get("url")
+        if url:
+
+            # Fetch Metadata For Thumbnail & title
+            page = metadata_parser.MetadataParser(
+                url=url, search_head_only=True)
+
+            fulltitle = page.get_metadatas(
+                'title', strategy=['page', 'og', 'dc', ])
+            thumbnail = page.get_metadata_link("image")
+
+            # Run Cli Cmd for Get Facebook data
+            command = f'youtube-dl "{url}" -j'
+            output = os.popen(command).read()
+            video_data = json.loads(output)
+
+            iRes = extract_facebook_video_data_from_url(video_data)
+            iRes["thumbnail"] = thumbnail
+            iRes["title"] = fulltitle
+
+            return Response(iRes)
+        return Response({"msg": "Please Provide Link"})
+
+    @api_view(['post'])
+    def get_bandcamp_videos(request, format=None):
+        """
+            Return a list of all users.
+            Doownload Video From Youtube-DL
+        """
+        url = request.data.get("url")
+        if url:
+            isTrack = "track" in url
+
+            if isTrack:
+                command = f'youtube-dl "{url}" -j'
+                output = os.popen(command).read()
+                video_data = json.loads(output)
+                iRes = extract_bandcamp_video_data_from_url(video_data)
+
+                return Response(iRes)
+
+            isAlbum = "album" in url
+            if isAlbum:
+                command = f'youtube-dl "{url}" -j'
+                output = os.popen(command).read()
+
+                splitStr = output.split("}\n")
+
+                video_data = get_arr_of_obj(splitStr)
+
+                albumData = {
+                    "thumbnail": video_data[0]["thumbnail"],
+                    "uploader": video_data[0]["uploader"],
+                    "artist": video_data[0]["artist"],
+                    "playlist_title": video_data[0]["playlist_title"],
+                    "album": video_data[0]["album"],
+                }
+
+                iRes = {**albumData, "all_videos": video_data}
+
+                return Response(iRes)
+
+            return Response({"msg": "Data Not Found!"})
+        return Response({"msg": "Please Provide Link!"})
