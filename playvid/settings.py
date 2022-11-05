9cd8ef3677ddeb24b954005b5a1da10855d59008
Ankit Maniya <ankit.maniya6588@gmail.com>
2022-11-05 16:55:48 +0530

    Frontend URL added

--- playvid/settings.py
+++ playvid/settings.py
@@ -136,8 +136,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "http://192.168.1.6:8000",
-    "https://f8eb-103-251-19-200.in.ngrok.io"
+    "https://playvid-fe.vercel.app"
 ]
 
 # Want forever-cacheable files and compression support
